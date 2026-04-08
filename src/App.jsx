fbf39d5062c6cde8109822666a6173b2357592dc
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-08 17:34:19 +0530

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/agence' element={<Agence />}/>
+        <Route path='/project' element={<Projects />} />
+      </Routes>
     </div>
   )
 }

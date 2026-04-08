ce3c39251610895e0a437668d3b84d0b2a8a1e2f
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-08 21:22:09 +0530

    add gsap

--- src/Components/Home/HomeHeroText.jsx
+++ src/Components/Home/HomeHeroText.jsx
@@ -5,13 +5,13 @@ import Video from './Video'
 const HomeHeroText = () => {
   return (
     <div className='font-[font2] pt-5 text-center '>
-        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[9vw]'>The spark for</div>
-        <div className='text-[9.5vw] uppercase flex items-start justify-center leading-[9vw]'>all 
+        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[9vw]'>L'étincelle</div>
+        <div className='text-[9.5vw] uppercase flex items-start justify-center leading-[9vw]'>qui 
             <div className='h-[8vw] w-[16vw] -mt-1  overflow-hidden rounded-full '>
              <Video />
             </div>
-            things</div>
-        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[9vw]'>creative</div>
+            génère</div>
+        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[9vw]'>la créativité</div>
     </div>
   )
 }
---
+++ src/Components/Navigation/FullScreenNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FullScreenNav() {
+  return (
+    <div>FullScreenNav</div>
+  )
+}
+
+export default FullScreenNav

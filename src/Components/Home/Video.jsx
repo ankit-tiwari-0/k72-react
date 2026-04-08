b0e22c70ccab59d85dc2a6c835375530fcb3a69a
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-08 19:21:58 +0530

    add Homeherotext of this website

--- src/Components/Home/Video.jsx
+++ src/Components/Home/Video.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div  className='h-full  w-full'>
-        <video  className="fixed top-0 left-0 w-screen h-screen object-cover" autoPlay loop muted
-      playsInline src="/video/video.mp4"></video>
+    <div className='h-full w-full'>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="/video/video.mp4"></video>
     </div>
   )
 }

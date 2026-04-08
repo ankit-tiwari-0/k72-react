7677e5969fef04eea0a732ed01eb9cdb14a231b4
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-08 19:53:31 +0530

    add  bottom of home

--- src/Components/Home/HomeHeroText.jsx
+++ src/Components/Home/HomeHeroText.jsx
@@ -5,13 +5,13 @@ import Video from './Video'
 const HomeHeroText = () => {
   return (
     <div className='font-[font2] pt-5 text-center '>
-        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[10vw]'>The spark for</div>
-        <div className='text-[9.5vw] uppercase flex items-start justify-center leading-[10vw]'>all 
-            <div className='h-[8vw] w-[16vw] -mt-2  overflow-hidden rounded-full '>
+        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[9vw]'>The spark for</div>
+        <div className='text-[9.5vw] uppercase flex items-start justify-center leading-[9vw]'>all 
+            <div className='h-[8vw] w-[16vw] -mt-1  overflow-hidden rounded-full '>
              <Video />
             </div>
             things</div>
-        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[10vw]'>creative</div>
+        <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[9vw]'>creative</div>
     </div>
   )
 }
